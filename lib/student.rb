4bb4d9f4e77aabc13f8ef7f38b8a08bae4355800
christian williams <christian.williamsj@gmail.com>
2019-11-20 07:04:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,6 +64,17 @@ def self.first_X_students_in_grade_10(number)
   end
 end
 
+  def seld.first_student_in_grade_10 
+    sql = <<-SQL 
+    SELECT * 
+    FROM students
+    WHERE grade = 10 
+    ORDER BY student.id LIMIT 1 
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
 def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
