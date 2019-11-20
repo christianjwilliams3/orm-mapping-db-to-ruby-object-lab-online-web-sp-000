07ccd39fcde6b772123c878ef1fe86b3921fe639
christian williams <christian.williamsj@gmail.com>
2019-11-20 07:07:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -76,6 +76,19 @@ end
       self.new_from_db(row)
     end.first
   end
+
+  def self.all_students_in_grade_X
+    sql = <<-SQL
+    SELECT * 
+    FROM students
+    WHERE grade = ?
+    ORDER BY students.id
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row) 
+  end
+end
 def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
