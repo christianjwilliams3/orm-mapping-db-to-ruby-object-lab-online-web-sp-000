03e7b6ef2d6301cd2631c96022ab2a35d6db160e
christian williams <christian.williamsj@gmail.com>
2019-11-20 06:43:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,6 +33,10 @@ class Student
   end.first
 end
 
+  def self.count_all_students_in_grade_9
+    sql = "SELECT * FROM students WHERE grade = 9"
+    DB[:conn].execute(sql)
+  end
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
